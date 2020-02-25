21e2fb092eecc50f35c040497b224a4f1f386217
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-02-25 16:19:43 -0500

    Create PharmacyWorkRequest.java

---
+++ HappyHealth/src/Business/WorkQueue/PharmacyWorkRequest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Harika
+ */
+public class PharmacyWorkRequest extends WorkRequest {
+    private int drugQuantity;
+    private String drugName;
+    private Date expectedDeliveryDate;
+
+    public Date getExpectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public void setExpectedDeliveryDate(Date expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+
+    public int getDrugQuantity() {
+        return drugQuantity;
+    }
+
+    public void setDrugQuantity(int drugQuantity) {
+        this.drugQuantity = drugQuantity;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return drugName;
+    }
+}
