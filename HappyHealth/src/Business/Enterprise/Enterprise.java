f8e56d4ba5abd7545f012f46bb3c258ea9699203
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-02-25 16:14:08 -0500

    Create Enterprise.java

---
+++ HappyHealth/src/Business/Enterprise/Enterprise.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+import Business.PharmacyInventory.PharmacyDirectory;
+
+/**
+ *
+ * @author ikyathvarmadantuluri
+ */
+public abstract class Enterprise extends Organization{
+   
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+    private PharmacyDirectory pharmacyDirectory;
+    
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+    public PharmacyDirectory getPharmacyDirectory() {
+        return pharmacyDirectory;
+    }
+
+    
+    
+    public enum EnterpriseType{
+        Hospital("Hospital"), Pharmacy("Pharmacy");
+        
+        
+        private String value;
+        
+        private EnterpriseType(String value){
+            this.value=value;
+        }
+        public String getValue() {
+            return value;
+        }
+        @Override
+        public String toString(){
+        return value;
+    }
+    }
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+    
+    public Enterprise(String name,EnterpriseType type){
+        super(name);
+        this.enterpriseType=type;
+        organizationDirectory=new OrganizationDirectory();
+        pharmacyDirectory = new PharmacyDirectory();
+        
+    }
+}
