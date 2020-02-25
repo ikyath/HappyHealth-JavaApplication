9a797b23d625da8baca3c79f3fc5233c2cb49557
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-02-25 16:14:16 -0500

    Create PharmacyEnterprise.java

---
+++ HappyHealth/src/Business/Enterprise/PharmacyEnterprise.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ikyathvarmadantuluri
+ */
+public class PharmacyEnterprise extends Enterprise {
+    
+    public PharmacyEnterprise(String name){
+        super(name,Enterprise.EnterpriseType.Pharmacy);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
