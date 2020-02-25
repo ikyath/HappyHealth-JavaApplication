b0fdf418ded6eda8d52a1c9c81aebc852b608f57
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-02-25 16:19:24 -0500

    Create EnterpriseAdminRole.java

---
+++ HappyHealth/src/Business/Role/EnterpriseAdminRole.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.InHomePatient.InHomePatient;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.ManageEnterpriseAdmin.EnterpriseAdminWorkAreaPanel;
+import userinterface.ManageMainAdmin.MainAdminWorkAreaPanel;
+
+/**
+ *
+ * @author ikyathvarmadantuluri
+ */
+public class EnterpriseAdminRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account,  Organization organization,Enterprise enterprise,Network network, EcoSystem system) {
+        return new EnterpriseAdminWorkAreaPanel(userProcessContainer, enterprise,network, account,system); 
+      }
+}
