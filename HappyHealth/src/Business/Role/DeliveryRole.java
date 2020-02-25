273bbc0c77e575bab3a997d452476ae7fbcfaef1
Satya Ikyath Varma Dantuluri <dantuluri.s@husky.neu.edu>
2020-02-25 16:19:20 -0500

    Create DeliveryRole.java

---
+++ HappyHealth/src/Business/Role/DeliveryRole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.DeliveryRole.DeliveryWorkAreaJPanel;
+
+/**
+ *
+ * @author Harika
+ */
+public class DeliveryRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,Network network, EcoSystem business) {
+        return new DeliveryWorkAreaJPanel(userProcessContainer, account, organization, enterprise, business, network);
+    }
+}
